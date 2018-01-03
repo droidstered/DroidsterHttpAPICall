6331216323ff59dc6f073901ff36faedd3b02ee3
Ravi Vachhani <droidster.me@gmail.com>
2018-01-03 17:37:16 +0530

    Seven Commit

--- app/src/main/java/me/droidsterhttpapicall/MainActivity.java
+++ app/src/main/java/me/droidsterhttpapicall/MainActivity.java
@@ -3,8 +3,6 @@ package me.droidsterhttpapicall;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.droidsterhttpcall.TestClass;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
