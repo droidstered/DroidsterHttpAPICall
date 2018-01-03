9a3ecf28d3db84374713c3dcc1763bd2c83dc722
Ravi Vachhani <droidster.me@gmail.com>
2018-01-03 17:10:05 +0530

    Initial commit

---
+++ droidsterhttpcall/src/main/java/com/example/droidsterhttpcall/TestClass.java
@@ -0,0 +1,18 @@
+package com.example.droidsterhttpcall;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Jetbro-4 on 03-01-2018.
+ */
+
+public class TestClass {
+
+
+    public  static  void ShowToast(Context context, String msg )
+    {
+        Toast.makeText(context,msg,Toast.LENGTH_SHORT).show();;
+    }
+
+}
